97f8301c600f2d6fe4da4038e6087e270f326550
ahmedfathydev <ahmed.fathy.dev@gmail.com>
2021-12-24 22:32:01 +0200

    Remove unnecessary code

--- AhlAlQuranAcademy.sql
+++ AhlAlQuranAcademy.sql
@@ -18,11 +18,10 @@ USE `AhlAlQuranAcademy` ;
 -- Table `AhlAlQuranAcademy`.`Level`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `AhlAlQuranAcademy`.`Level` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
+  `Id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `Name` NVARCHAR(50) NOT NULL,
   `Discription` NVARCHAR(250) NULL,
-  `Capacity` INT(4) NOT NULL,
-  PRIMARY KEY (`Id`))
+  `Capacity` INT(4) NOT NULL)
 ENGINE = InnoDB;
 
 
@@ -30,15 +29,13 @@ ENGINE = InnoDB;
 -- Table `AhlAlQuranAcademy`.`Student`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `AhlAlQuranAcademy`.`Student` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
+  `Id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `FirstName` NVARCHAR(50) NOT NULL,
   `LastName` NVARCHAR(50) NOT NULL,
   `Telegram` VARCHAR(25) NOT NULL,
-  `Email` VARCHAR(250) NOT NULL,
+  `Email` VARCHAR(250) NOT NULL UNIQUE,
   `RedFlag` INT(1) NOT NULL DEFAULT 0,
-  `Passwoed` VARCHAR(250) NOT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE INDEX `Email_UNIQUE` (`Email` ASC) VISIBLE)
+  `Passwoed` VARCHAR(250) NOT NULL)
 ENGINE = InnoDB;
 
 
@@ -49,7 +46,6 @@ CREATE TABLE IF NOT EXISTS `AhlAlQuranAcademy`.`StudentLevel` (
   `LevelId` INT NOT NULL,
   `StudentId` INT NOT NULL,
   `Score` INT NOT NULL,
-  INDEX `FK_StudentLevel_Student_IDX` (`StudentId` ASC) VISIBLE,
   PRIMARY KEY (`LevelId`, `StudentId`),
   CONSTRAINT `FK_StudentLevel_Level`
     FOREIGN KEY (`LevelId`)
@@ -68,16 +64,13 @@ ENGINE = InnoDB;
 -- Table `AhlAlQuranAcademy`.`Sheikh`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `AhlAlQuranAcademy`.`Sheikh` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
+  `Id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `FirstName` NVARCHAR(50) NOT NULL,
   `LastName` NVARCHAR(50) NOT NULL,
-  `Email` VARCHAR(250) NOT NULL,
-  `Telegram` VARCHAR(25) NOT NULL,
+  `Email` VARCHAR(250) NOT NULL UNIQUE,
+  `Telegram` VARCHAR(25) NOT NULL UNIQUE,
   `Policy` VARCHAR(3) NOT NULL,
-  `Rate` DECIMAL(1,1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`Id`),
-  UNIQUE INDEX `Email_UNIQUE` (`Email` ASC) VISIBLE,
-  UNIQUE INDEX `Telegram_UNIQUE` (`Telegram` ASC) VISIBLE)
+  `Rate` DECIMAL(1,1) NOT NULL DEFAULT 0)
 ENGINE = InnoDB;
 
 
@@ -85,14 +78,12 @@ ENGINE = InnoDB;
 -- Table `AhlAlQuranAcademy`.`Room`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `AhlAlQuranAcademy`.`Room` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
+  `Id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `Name` NVARCHAR(50) NOT NULL,
   `Discription` NVARCHAR(250) NULL,
   `Capacity` INT(2) NOT NULL DEFAULT 5,
   `Type` INT(1) NOT NULL,
   `SheikhId` INT NOT NULL,
-  PRIMARY KEY (`Id`),
-  INDEX `FK_Room_Sheikh_IDX` (`SheikhId` ASC) VISIBLE,
   CONSTRAINT `FK_Room_Sheikh`
     FOREIGN KEY (`SheikhId`)
     REFERENCES `AhlAlQuranAcademy`.`Sheikh` (`Id`)
@@ -107,8 +98,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `AhlAlQuranAcademy`.`StudentRoom` (
   `StudentId` INT NOT NULL,
   `RoomId` INT NOT NULL,
-  INDEX `FK_StudentRoom_Student_IDX` (`StudentId` ASC) INVISIBLE,
-  INDEX `FK_StudentRoom_Room_IDX` (`RoomId` ASC) VISIBLE,
+  PRIMARY KEY (`StudentId`, `RoomId`),
   CONSTRAINT `FK_StudentRoom_Student`
     FOREIGN KEY (`StudentId`)
     REFERENCES `AhlAlQuranAcademy`.`Student` (`Id`)
@@ -126,14 +116,11 @@ ENGINE = InnoDB;
 -- Table `AhlAlQuranAcademy`.`Lesson`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `AhlAlQuranAcademy`.`Lesson` (
-  `Id` INT NOT NULL,
+  `Id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `Name` NVARCHAR(50) NULL,
   `Content` NVARCHAR(250) NULL,
   `LevelId` INT NOT NULL,
   `SheikhId` INT NOT NULL,
-  PRIMARY KEY (`Id`),
-  INDEX `FK_lesson_Level_IDX` (`LevelId` ASC) INVISIBLE,
-  INDEX `FK_Lesson_Sheikh_IDX` (`SheikhId` ASC) VISIBLE,
   CONSTRAINT `FK_lesson_Level`
     FOREIGN KEY (`LevelId`)
     REFERENCES `AhlAlQuranAcademy`.`Level` (`Id`)
@@ -151,10 +138,9 @@ ENGINE = InnoDB;
 -- Table `AhlAlQuranAcademy`.`TempSheikh`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `AhlAlQuranAcademy`.`TempSheikh` (
-  `SheikhId` INT NOT NULL,
+  `SheikhId` INT NOT NULL PRIMARY KEY,
   `FirstName` NVARCHAR(50) NOT NULL,
-  `LastName` NVARCHAR(50) NOT NULL,
-  PRIMARY KEY (`SheikhId`))
+  `LastName` NVARCHAR(50) NOT NULL)
 ENGINE = InnoDB;
 
 
@@ -162,14 +148,12 @@ ENGINE = InnoDB;
 -- Table `AhlAlQuranAcademy`.`News`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `AhlAlQuranAcademy`.`News` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
+  `Id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `Title` NVARCHAR(50) NOT NULL,
   `ContentPath` VARCHAR(45) NOT NULL,
   `TimeStamp` DATETIME NOT NULL,
   `Edited` TINYINT NOT NULL DEFAULT 0,
   `SheikhId` INT NOT NULL,
-  PRIMARY KEY (`Id`),
-  INDEX `FK_News_Sheikh_IDX` (`SheikhId` ASC) INVISIBLE,
   CONSTRAINT `FK_News_Sheikh`
     FOREIGN KEY (`SheikhId`)
     REFERENCES `AhlAlQuranAcademy`.`Sheikh` (`Id`)
